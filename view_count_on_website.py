61f94b2ea20c1b6c8ada26c6fcfdd29a0269e83a
farooqui <farooquee.faheem011@gmail.com>
2020-07-18 18:00:26 +0530

    fuction for login

--- view_count_on_website.py
+++ view_count_on_website.py
@@ -1,13 +1,33 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support.ui import WebDriverWait 
+from time import sleep
 
-driver=webdriver.Chrome("C:/Users/faroo/Desktop/work/drivers/chromedriver.exe")
 
-website_url="https://www.newsgallery.com/"
-driver.get(website_url)
+class NewsGallery:
 
-refreshrate=int(15)
-#this would keep running untill you stop the compiler
-while True:
-    time.sleep(refreshrate)
-    driver.refresh()
+    def __init__(self,username,password):
+        #these lines will help if someone faces issues like 
+        # chrome closes after execution 
+        # self.opts=webdriver.ChromeOptions()
+        # self.opts.add_experimental_option("detach",True)
+        self.driver=webdriver.Chrome("C:/Users/faroo/Desktop/work/drivers/chromedriver.exe")
+        self.driver.maximize_window()
+
+        #username passwrod
+        self.username=username
+        self.password=password
+
+        #open Newsgallery login page
+        self.driver.get("https://www.newsgallery.com/")
+        sleep(2)
+
+        #automatically enter username and password
+        self.driver.find_element(By.ID,"guest-login-button").click()
+        self.driver.find_element(By.ID,"id_username").send_keys(self.username)
+        self.driver.find_element(By.ID,"id_password").send_keys(self.password)
+        #click on login button
+        self.driver.find_element(By.ID,"login_button").click()
+
+obj1=NewsGallery("xxxxx","xxxxxxxxxx")
